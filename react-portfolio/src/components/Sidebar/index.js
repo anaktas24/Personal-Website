86652b0f8c703ea5bb260ad8b97fede1d0a4ec35
Ioannis Antoniou <johnantony24@hotmail.com>
2023-01-20 07:15:22 +0100

    sidebar social

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from'../../assets/images/logo-s.png'
 import LogoSubtitle from'../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -28,11 +29,18 @@ const Sidebar = () => (
       <NavLink
         exact="true"
         activeclassname="active"
-        className="cotact-link"
+        className="contact-link"
         to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a target='blank' rel='noreferrer' href='https://www.linkedin.com/in/ioanton/'>
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+        </a>
+      </li>
+    </ul>
   </div>
 
 )
