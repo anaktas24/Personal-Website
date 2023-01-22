ab88f397904989ee0fb7d174b485cfa7b9d3d6ca
Ioannis Antoniou <johnantony24@hotmail.com>
2023-01-22 18:01:34 +0100

    contact input form

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import '/.index.scss'
+import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 
@@ -13,15 +13,39 @@ const Contact = () => {
 
   return(
     <>
-      <div className="container about-page">
+      <div className="container contact-page">
         <div className="text-zone">
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't']}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
               idx={15}
             />
           </h1>
+          <p>
+            I'm a very ambitious front-end developer looking for a role in an
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+        </div>
+        <div className='contact-form'>
+          <ul>
+            <li className='half'>
+              <input type='text' name='name' placeholder='Name' required/>
+            </li>
+            <li className='half'>
+              <input type='email' name='email' placeholder='Email' required/>
+            </li>
+            <li>
+              <input type='text' name='subject' placeholder='Subject' required/>
+            </li>
+            <li>
+              <textarea placeholder='Message' name='message' required> </textarea>
+            </li>
+            <li>
+              <input type='submit' className='flat-button' value='SEND'/>
+            </li>
+          </ul>
         </div>
       </div>
     </>
