232e330190b7d7620d6941071ecc041c04e567a3
Ioannis Antoniou <johnantony24@hotmail.com>
2023-01-22 17:13:59 +0100

    about route

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,5 +1,24 @@
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
-const About = () => 'Hello'
+const About = () => {
+
+  return(
+    <div className='container abou-page'>
+      <div className='text-zone'>
+        <h1>
+          <AnimatedLetters
+            strArray={['About me'].split}
+            idx={15}
+          />
+        </h1>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+
+    </div>
+  )
+}
 
 export default About
