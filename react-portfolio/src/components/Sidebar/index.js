c0d1fd9b438e4a581aee1e1769d78606623421d9
Ioannis Antoniou <johnantony24@hotmail.com>
2023-01-24 18:24:27 +0100

    Portfolio page

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import LogoS from'../../assets/images/logo-s.png'
 import LogoSubtitle from'../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
@@ -13,10 +13,13 @@ const Sidebar = () => (
   <div className='nav-bar'>
     <Link className='logo' to='/'>
         <img src={LogoS} alt="logo" />
-        <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
+        <img className='sub-logo' src={LogoSubtitle} alt="Ioannis" />
     </Link>
     <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
       <NavLink
@@ -25,6 +28,13 @@ const Sidebar = () => (
         className="about-link"
         to="/about">
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="portfolio-link"
+        to="/portfolio">
+        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
       </NavLink>
       <NavLink
         exact="true"
