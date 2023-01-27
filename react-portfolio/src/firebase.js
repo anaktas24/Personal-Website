849220f30f90ed18890e145db27b06b5ca57cc58
Ioannis Antoniou <johnantony24@hotmail.com>
2023-01-27 11:54:21 +0100

    firebase config

--- react-portfolio/src/firebase.js
+++ react-portfolio/src/firebase.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore/lite';
+import '.firebaseconfig.js'
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDFLL398A6dlA3c3nzMaetbmVufhCUvEVQ",
+  authDomain: "react-portfolio-dashboar-413e0.firebaseapp.com",
+  projectId: "react-portfolio-dashboar-413e0",
+  storageBucket: "react-portfolio-dashboar-413e0.appspot.com",
+  messagingSenderId: "141598938627",
+  appId: "1:141598938627:web:1601d69df260a42c186036",
+  measurementId: "G-F108YZFXCT"
 };
 
 
