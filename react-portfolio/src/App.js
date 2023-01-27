015f20c576d220951762fa9d408b705d6dfa7eb5
Ioannis Antoniou <johnantony24@hotmail.com>
2023-01-27 11:42:01 +0100

    dashboard/home page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,7 +4,7 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import Portfolio from './components/Portfolio'
-// import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard'
 import './App.scss'
 import './config.js'
 
@@ -18,7 +18,7 @@ function App() {
           <Route path="about" element={<About/>} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
     </>
--- react-portfolio/src/firebase.js
+++ react-portfolio/src/firebase.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore/lite';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFLL398A6dlA3c3nzMaetbmVufhCUvEVQ",
-  authDomain: "react-portfolio-dashboar-413e0.firebaseapp.com",
-  projectId: "react-portfolio-dashboar-413e0",
-  storageBucket: "react-portfolio-dashboar-413e0.appspot.com",
-  messagingSenderId: "141598938627",
-  appId: "1:141598938627:web:1601d69df260a42c186036",
-  measurementId: "G-F108YZFXCT"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth();
+const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
